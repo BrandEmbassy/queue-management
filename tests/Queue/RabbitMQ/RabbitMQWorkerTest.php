d5df3a189dab1789a550550e4e38b724d24d57cc
Dominik Voda <d.voda94@gmail.com>
2019-06-12 16:35:18 +0200

    + ConnectionFactory test + RabbitMQWorker test

---
+++ tests/Queue/RabbitMQ/RabbitMQWorkerTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\BE\QueueManagement\Queue\RabbitMQ;
+
+use BE\QueueManagement\Queue\RabbitMQ\RabbitMQConsumer;
+use BE\QueueManagement\Queue\RabbitMQ\RabbitMQQueueManager;
+use BE\QueueManagement\Queue\RabbitMQ\RabbitMQWorker;
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+use Tests\BE\QueueManagement\Jobs\JobDefinitions\DummyJobDefinition;
+
+class RabbitMQWorkerTest extends TestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    /**
+     * @var RabbitMQQueueManager|MockInterface
+     */
+    private $rabbitMQQueueManagerMock;
+
+    /**
+     * @var RabbitMQConsumer|MockInterface
+     */
+    private $rabbitMQConsumerMock;
+
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->rabbitMQQueueManagerMock = Mockery::mock(RabbitMQQueueManager::class);
+        $this->rabbitMQConsumerMock = Mockery::mock(RabbitMQConsumer::class);
+    }
+
+
+    public function testStart(): void
+    {
+        $rabbitMQWorker = $this->createRabbitMqWorker();
+
+        $customConsumeParameters = ['foo' => 'bar'];
+
+        $this->rabbitMQQueueManagerMock->shouldReceive('consumeMessages')
+            ->with($this->rabbitMQConsumerMock, DummyJobDefinition::QUEUE_NAME, $customConsumeParameters)
+            ->once();
+
+        $rabbitMQWorker->start(DummyJobDefinition::QUEUE_NAME, $customConsumeParameters);
+    }
+
+
+    private function createRabbitMqWorker(): RabbitMQWorker
+    {
+        return new RabbitMQWorker($this->rabbitMQQueueManagerMock, $this->rabbitMQConsumerMock);
+    }
+}
