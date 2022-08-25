4cb8b248fd6a5f56266c71c518f77dafe52d13f9
Petr Heinz <petr.heinz@niceincontact.com>
2022-08-25 11:12:44 +0200

    SqsQueueManager: extract CONSUME_LOOP_ITERATIONS_NO_LIMIT

--- src/Queue/AWSSQS/SqsQueueManager.php
+++ src/Queue/AWSSQS/SqsQueueManager.php
@@ -29,6 +29,8 @@ class SqsQueueManager implements QueueManagerInterface
      * Valid values: 1 to 10. Default: 1.
      */
     public const MAX_NUMBER_OF_MESSAGES = 'MaxNumberOfMessages';
+    public const CONSUME_LOOP_ITERATIONS_NO_LIMIT = -1;
+
     private const WAIT_TIME_SECONDS = 'WaitTimeSeconds';
     private const DELAY_SECONDS = 'DelaySeconds';
     // SQS allows maximum message delay of 15 minutes
@@ -48,9 +50,6 @@ class SqsQueueManager implements QueueManagerInterface
 
     private LoggerInterface $logger;
 
-    /**
-     * -1 = no limit
-     */
     private int $consumeLoopIterationsCount;
 
 
@@ -60,7 +59,7 @@ class SqsQueueManager implements QueueManagerInterface
         S3ClientFactoryInterface $s3ClientFactory,
         MessageKeyGeneratorInterface $messageKeyGenerator,
         LoggerInterface $logger,
-        int $consumeLoopIterationsCount = -1
+        int $consumeLoopIterationsCount = self::CONSUME_LOOP_ITERATIONS_NO_LIMIT
     ) {
         $this->s3BucketName = $s3BucketName;
         $this->sqsClientFactory = $sqsClientFactory;
@@ -146,9 +145,9 @@ class SqsQueueManager implements QueueManagerInterface
         $waitTimeSeconds = (int)($parameters[self::WAIT_TIME_SECONDS] ?? 10);
 
         $loopIterationsCounter = 0;
-        $consumeLoopIterationsCount = $this->getConsumeLoopIterationsCount();
+        $isLoopIterationsLimitEnabled = $this->consumeLoopIterationsCount !== self::CONSUME_LOOP_ITERATIONS_NO_LIMIT;
 
-        while (($loopIterationsCounter < $consumeLoopIterationsCount) || $consumeLoopIterationsCount === -1) {
+        while (!$isLoopIterationsLimitEnabled || $loopIterationsCounter < $this->consumeLoopIterationsCount) {
             try {
                 $result = $this->sqsClient->receiveMessage([
                     'AttributeNames' => ['All'],
@@ -165,7 +164,7 @@ class SqsQueueManager implements QueueManagerInterface
                         $consumer($sqsMessage);
                     }
                 }
-                if ($this->consumeLoopIterationsCount !== -1) {
+                if ($isLoopIterationsLimitEnabled) {
                     $loopIterationsCounter++;
                 }
             } catch (AwsException $exception) {
