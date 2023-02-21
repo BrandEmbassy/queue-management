a88cfa20ffe73e68f63921822f2ac659c532eb31
dominikvoda <d.voda94@gmail.com>
2023-02-21 16:09:13 +0100

    Work only with job parameters in serialization

--- tests/Jobs/ExampleJobTest.php
+++ tests/Jobs/ExampleJobTest.php
@@ -38,6 +38,7 @@ class ExampleJobTest extends TestCase
             JobParameters::ATTEMPTS => 1,
             JobParameters::CREATED_AT => self::JOB_CREATED_AT,
             JobParameters::PARAMETERS => ['foo' => 'bar'],
+            JobParameters::EXECUTION_PLANNED_AT => null,
         ];
 
         DateTimeAssertions::assertDateTimeAtomEqualsDateTime(self::JOB_CREATED_AT, $simpleJob->getCreatedAt());
